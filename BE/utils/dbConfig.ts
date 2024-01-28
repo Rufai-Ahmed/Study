a33ec716d78cb552bbd05687e2d5ccf589f72828
Rufai-Ahmed <abbeyrufai234@gmail.com>
2024-01-28 14:22:34 +0100

    readying for hosting

--- BE/utils/dbConfig.ts
+++ BE/utils/dbConfig.ts
@@ -1,17 +1,14 @@
-import {connect} from 'mongoose'
+import { connect } from "mongoose";
 
-const URL: string = 'mongodb://localhost:27017/studyDB'
+const URL: string =
+  "mongodb+srv://abbeyrufai234:abbeyrufai234@cluster0.yokwex4.mongodb.net/studyDB?retryWrites=true&w=majority";
 
 export const dbConfig = async () => {
-    try {
-        
-        return await connect(URL).then(()=>{
-            console.log("DB connected");
-            
-        })
-
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
+  try {
+    return await connect(URL).then(() => {
+      console.log("DB connected");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
